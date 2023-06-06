44ad203677f91dc388d3766ce4d7cc996d19b234
SenatorDingo <abhayabhilash@duck.com>
2023-06-06 09:31:34 -0400

    Added a Login Class and linked the Login Button

--- app/src/main/java/com/example/tutronapp/MainActivity.java
+++ app/src/main/java/com/example/tutronapp/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.tutronapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+     private Button btnLogin, btnSignup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.btnLogin);
+        btnSignup = findViewById(R.id.btnSignup);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }

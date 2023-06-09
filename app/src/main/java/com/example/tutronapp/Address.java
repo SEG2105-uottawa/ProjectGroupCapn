19bfc9734dbffc0ce9bfc4dd57e5c41bea2ba14c
SenatorDingo <abhayabhilash@duck.com>
2023-06-08 20:21:21 -0400

    Added getters setters etc for address

--- app/src/main/java/com/example/tutronapp/Address.java
+++ app/src/main/java/com/example/tutronapp/Address.java
@@ -5,7 +5,36 @@ public class Address {
     private String postCode;
     private int streetNumber;
     private String streetName;
-    public Address(){
+    public Address() {
 
+    }
+    public Address(String postCode, int streetNumber, String streetName){
+        this.postCode = postCode;
+        this.streetNumber = streetNumber;
+        this.streetName = streetName;
+    }
+
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(String postCode) {
+        this.postCode = postCode;
+    }
+
+    public int getStreetNumber() {
+        return streetNumber;
+    }
+
+    public void setStreetNumber(int streetNumber) {
+        this.streetNumber = streetNumber;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
     }
 }

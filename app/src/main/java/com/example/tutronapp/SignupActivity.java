de64bc37bbba088dad62d17e5c1a9b713dbca9ce
Shuail135 <sumyanwan@gmail.com>
2023-06-10 17:22:53 -0400

    fixed bug

--- app/src/main/java/com/example/tutronapp/SignupActivity.java
+++ app/src/main/java/com/example/tutronapp/SignupActivity.java
@@ -219,15 +219,10 @@ public class SignupActivity extends AppCompatActivity {
             return false;
         }
 
-        // Check if there is at least one "@" and one "."
+        // Check if there is at least one "@" and ".", or there is more than one "@"
         int atIndex = email.indexOf("@");
-        int dotIndex = email.lastIndexOf(".");
-        if (atIndex == -1 || dotIndex == -1) {
-            return false;
-        }
-
-        // Check if there is more than one "@" or "."
-        if (email.indexOf("@", atIndex + 1) != -1 || email.indexOf(".", dotIndex + 1) != -1) {
+        int dotIndex = email.indexOf(".");
+        if (atIndex == -1 || dotIndex == -1 || email.indexOf("@", atIndex + 1) != -1) {
             return false;
         }
 
@@ -237,8 +232,8 @@ public class SignupActivity extends AppCompatActivity {
         }
 
         // Check if there are two dots in the domain part
-        int secondDotIndex = email.indexOf(".", atIndex + 1);
-        if (secondDotIndex != -1 && secondDotIndex < dotIndex - 1) {
+        int secondDotIndex = email.indexOf(".", dotIndex+1);
+        if (secondDotIndex != -1 && secondDotIndex != email.lastIndexOf(".")) {
             return false;
         }
 

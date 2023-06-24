d8738605f31e259441bd4cd846f6a306bb491bb8
Nalan Kurnaz <nkurn026@uottawa.ca>
2023-06-24 15:11:16 -0400

    Only ComplaintTest and ComplaintListTest need to be implemented

--- app/src/test/java/com/example/tutronapp/ExampleUnitTest.java
+++ app/src/test/java/com/example/tutronapp/ExampleUnitTest.java
@@ -1,5 +1,6 @@
 package com.example.tutronapp;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,6 +11,22 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+    //this is going to test all of the files at the same time
+
+    public static void main(String[] args) {
+        TestUtil.runClass(AddressTest.class);
+        TestUtil.runClass(AdministratorTest.class);
+        //TestUtil.runClass(ComplaintListTest.class);
+        //TestUtil.runClass(ComplaintTest.class);
+        TestUtil.runClass(CreditCardTest.class);
+        TestUtil.runClass(PersonTest.class);
+        TestUtil.runClass(StudentTest.class);
+        TestUtil.runClass(TutorTest.class);
+        TestUtil.runClass(UserTest.class);
+    }
+
+
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);

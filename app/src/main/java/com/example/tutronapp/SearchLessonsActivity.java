17fdac054a46202d11686479803857354b86f9a4
SenatorDingo <abhayabhilash@duck.com>
2023-07-12 13:32:19 -0400

    Completed the Layout of the Search Page

--- app/src/main/java/com/example/tutronapp/SearchLessonsActivity.java
+++ app/src/main/java/com/example/tutronapp/SearchLessonsActivity.java
@@ -11,6 +11,7 @@ public class SearchLessonsActivity extends AppCompatActivity {
 
     private DatabaseReference allOfferedTopics;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

4badaa1975023083a00322408e585556b8a40578
SenatorDingo <abhayabhilash@duck.com>
2023-07-12 10:48:49 -0400

    Updated StudentHomepageActivity Added SearchLessonsActivity

---
+++ app/src/main/java/com/example/tutronapp/SearchLessonsActivity.java
@@ -0,0 +1,14 @@
+package com.example.tutronapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchLessonsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_lessons);
+    }
+}
--- app/src/main/java/com/example/tutronapp/StudentHomepageActivity.java
+++ app/src/main/java/com/example/tutronapp/StudentHomepageActivity.java
@@ -1,12 +1,18 @@
 package com.example.tutronapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StudentHomepageActivity extends AppCompatActivity {
 
     private Student loggedInStudent;
+    private Button btnViewPurchaseStatus, btnPurchaseLesson;
+    private RecyclerView recyclerViewYourLessons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +23,18 @@ public class StudentHomepageActivity extends AppCompatActivity {
         if (bundle != null && bundle.containsKey("Student")) {
             loggedInStudent = (Student) bundle.getSerializable("Student");
         }
+
+        btnPurchaseLesson = findViewById(R.id.btnPurchaseLesson);
+        btnViewPurchaseStatus = findViewById(R.id.btnViewPurchaseStatus);
+        recyclerViewYourLessons = findViewById(R.id.recyclerViewYourLessons);
+
+
+
+
+        btnPurchaseLesson.setOnClickListener(v -> {
+            Intent intent = new Intent(StudentHomepageActivity.this, SearchLessonsActivity.class);
+            startActivity(intent);
+        });
 
     }
 }

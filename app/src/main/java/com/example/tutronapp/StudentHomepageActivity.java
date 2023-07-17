921aebdeb95959adf39c382be51e4a5e9243685b
SenatorDingo <abhayabhilash@duck.com>
2023-07-16 21:56:12 -0400

    Notification Yaaay!!!!!

--- app/src/main/java/com/example/tutronapp/StudentHomepageActivity.java
+++ app/src/main/java/com/example/tutronapp/StudentHomepageActivity.java
@@ -2,13 +2,17 @@ package com.example.tutronapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,6 +24,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.List;
@@ -44,6 +49,25 @@ public class StudentHomepageActivity extends AppCompatActivity {
         if (bundle != null && bundle.containsKey("Student")) {
             loggedInStudent = (Student) bundle.getSerializable("Student");
         }
+        Query getRelatedNotification = databaseReference.child("notifications").orderByChild("studentID").equalTo(loggedInStudent.getDataBaseID());
+        getRelatedNotification.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot notificationSnapshot : dataSnapshot.getChildren()) {
+                    PurchaseNotification notification = notificationSnapshot.getValue(PurchaseNotification.class);
+
+                    showBasicNotification(StudentHomepageActivity.this, "Approved", notification.getNotificationText());
+
+                    notificationSnapshot.getRef().removeValue();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
 
         DatabaseReference usersRef = FirebaseDatabase.getInstance().getReference().child("users");
 
@@ -168,4 +192,27 @@ public class StudentHomepageActivity extends AppCompatActivity {
         });
         dialogFragment.show(getSupportFragmentManager(), "RatingDialogFragment");
     }
+
+    private void showBasicNotification(Context context, String title, String message) {
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String channelId = "tutron_channel";
+            String channelName = "Tutron Channel";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+
+            NotificationChannel channel = new NotificationChannel(channelId, channelName, importance);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "tutron_channel")
+                .setSmallIcon(R.drawable.ic_notification)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        int notificationId = 1;
+        notificationManager.notify(notificationId, builder.build());
+    }
+
 }

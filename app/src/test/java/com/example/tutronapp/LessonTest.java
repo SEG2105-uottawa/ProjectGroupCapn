342af47e974545b283ef0d5b73f8823e800c21a9
Nalan Kurnaz <nkurn026@uottawa.ca>
2023-07-16 21:34:36 -0400

    Implementation of LessonTest

--- app/src/test/java/com/example/tutronapp/LessonTest.java
+++ app/src/test/java/com/example/tutronapp/LessonTest.java
@@ -1,64 +1,95 @@
 package com.example.tutronapp;
 
-import org.junit.Before;
+import org.junit.Assert;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 public class LessonTest {
 
-    private Lesson lesson;
-    private Topic topic;
-    private Tutor tutor;
+    @Test
+    public void testEquals() {
+        // Create two Lesson objects with the same properties
+        Topic topic = new Topic("Math", "tutor1", 5, "Mathematics topic");
+        Long date = System.currentTimeMillis();
+        Tutor tutor = new Tutor("John", "Doe", "john.doe@example.com", "password");
+        String studentId = "123";
+        String studentName = "Alice";
 
-    @Before
-    public void setUp() {
-        topic = new Topic("Math", "tutor123", 5, "Algebra");
-        tutor = new Tutor("John", "Doe", "john.doe@example.com", "password", "PhD", "English", "Experienced tutor");
-        lesson = new Lesson(topic, 1626349200L, tutor, "student123", "Alice");
+        Lesson lesson1 = new Lesson(topic, date, tutor, studentId, studentName);
+        Lesson lesson2 = new Lesson(topic, date, tutor, studentId, studentName);
+
+        // Assert that the two Lesson objects are equal
+        Assert.assertEquals(lesson1, lesson2);
     }
 
     @Test
-    public void testGetTopicToBeTaught() {
-        assertEquals(topic, lesson.getTopicToBeTaught());
+    public void testNotEquals() {
+        // Create two Lesson objects with different properties
+        Topic topic1 = new Topic("Math", "tutor1", 5, "Mathematics topic");
+        Long date1 = System.currentTimeMillis();
+        Tutor tutor1 = new Tutor("John", "Doe", "john.doe@example.com", "password");
+        String studentId1 = "123";
+        String studentName1 = "Alice";
+
+        Topic topic2 = new Topic("Science", "tutor2", 3, "Science topic");
+        Long date2 = System.currentTimeMillis() + 1000;
+        Tutor tutor2 = new Tutor("Jane", "Smith", "jane.smith@example.com", "password");
+        String studentId2 = "456";
+        String studentName2 = "Bob";
+
+        Lesson lesson1 = new Lesson(topic1, date1, tutor1, studentId1, studentName1);
+        Lesson lesson2 = new Lesson(topic2, date2, tutor2, studentId2, studentName2);
+
+        // Assert that the two Lesson objects are not equal
+        Assert.assertNotEquals(lesson1, lesson2);
     }
 
     @Test
-    public void testGetDateOfLesson() {
-        assertEquals(Long.valueOf(1626349200L), lesson.getDateOfLesson());
+    public void testLessonTutor() {
+        // Create a Tutor object
+        Tutor tutor = new Tutor("John", "Doe", "john.doe@example.com", "password");
+
+        // Create a Lesson object
+        Topic topic = new Topic("Math", "tutor1", 5, "Mathematics topic");
+        Long date = System.currentTimeMillis();
+        String studentId = "123";
+        String studentName = "Alice";
+        Lesson lesson = new Lesson(topic, date, tutor, studentId, studentName);
+
+        // Assert that the Tutor in the Lesson is the same as the created Tutor object
+        Assert.assertEquals(tutor, lesson.getTutorTeaching());
     }
 
     @Test
-    public void testGetStudentDatabaseID() {
-        assertEquals("student123", lesson.getStudentDatabaseID());
+    public void testTutorRating() {
+        // Create a Tutor object
+        Tutor tutor = new Tutor("John", "Doe", "john.doe@example.com", "password");
+
+        // Add ratings to the Tutor
+        tutor.addRating(4);
+        tutor.addRating(5);
+
+        // Assert that the Tutor's rating is correctly calculated
+        Assert.assertEquals(4.5, tutor.getRating(), 0.01);
     }
 
     @Test
-    public void testGetDatabaseID() {
-        assertNull(lesson.getDatabaseID());
-    }
+    public void testTutorRatingWithChange() {
+        // Create a Tutor object
+        Tutor tutor = new Tutor("John", "Doe", "john.doe@example.com", "password");
 
-    @Test
-    public void testSetDatabaseID() {
-        lesson.setDatabaseID("lesson1");
-        assertEquals("lesson1", lesson.getDatabaseID());
-    }
+        // Add ratings to the Tutor
+        tutor.addRating(4);
+        tutor.addRating(5);
 
-    @Test
-    public void testGetTutorTeaching() {
-        assertEquals(tutor, lesson.getTutorTeaching());
-    }
+        // Change a rating
+        tutor.changeCurrentRating(4, 3);
 
-    @Test
-    public void testGetStudentName() {
-        assertEquals("Alice", lesson.getStudentName());
-    }
-
-    @Test
-    public void testSetStudentName() {
-        lesson.setStudentName("Bob");
-        assertEquals("Bob", lesson.getStudentName());
+        // Assert that the Tutor's rating is correctly updated after the change
+        Assert.assertEquals(4.0, tutor.getRating(), 0.01);
     }
 }
+
+
 
 
 
